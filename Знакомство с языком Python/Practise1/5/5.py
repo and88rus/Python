a38ccdda1021e5d201dd59bca6d602efff9b2871
and88rus <nest160802@gmail.com>
2022-09-05 23:48:26 +0300

    сделал 5-ое задание

--- Знакомство с языком Python/Practise1/5/5.py
+++ Знакомство с языком Python/Practise1/5/5.py
@@ -1,15 +1,15 @@
+import math
 def print_hi():
     print()
 if __name__ == '__main__':
     print_hi()
-    from math import sqrt
     def DistanceBetween2Points(x1,x2,y1,y2):
-        d=sqrt((x2-x1)**2+((y2-y1)**2))
+        d=(math.pow((x2-x1),2)+math.pow((y2-y1),2))**0.5
         return d
     print ("Введите координаты точки A")
-    x1 = input("x=")
-    y1 = input("y=")
+    x1 = int(input("x1="))
+    y1 = int(input("y1="))
     print ("Введите координаты точки B")
-    x2 = input("x=")
-    y2 = input("y=")
-    print (DistanceBetween2Points(x1,y1,x2,y2))
+    x2 = int(input("x2="))
+    y2 = int(input("y2="))
+    print (DistanceBetween2Points(x1,x2,y1,y2))
