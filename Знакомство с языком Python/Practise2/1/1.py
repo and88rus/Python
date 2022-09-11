a91a1aa56ac6bbc8c3c563d65acad492faa87104
and88rus <nest160802@gmail.com>
2022-09-11 22:13:03 +0300

    переделал 2-ое и сделал 3-ее задание

--- Знакомство с языком Python/Practise2/1/1.py
+++ Знакомство с языком Python/Practise2/1/1.py
@@ -2,13 +2,4 @@ def print_hi():
     print()
 if __name__ == '__main__':
     print_hi()
-    from array import *
-    s = input("N=")
-    p=[]
-    m = array('i', (0 for i in range(0, int(s))))
-    for i in range(int(s)): m[i] = i+1
-    sum=1
-    for i in range(int(s)):
-        sum*=m[i]
-        p.append(sum)
-    print(p)
+    
