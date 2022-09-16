03965c1de1e362291ed3caf7490ae57a010cbd20
and88rus <nest160802@gmail.com>
2022-09-16 13:56:09 +0300

    Сделал 5-ое задание

--- Знакомство с языком Python/Practise2/5/5.py
+++ Знакомство с языком Python/Practise2/5/5.py
@@ -1,17 +1,20 @@
-from array import*
+import random
 def print_hi():
     print()
 if __name__ == '__main__':
     print_hi()
-def SumOfSymb(a):
-    c=a
-    size =1 
-    while ((a>=10)):
-        a=a/10
-        size=size+1
-    m = array('i', (0 for i in range(0, size)))
-    for i in range(size-1,0,1): 
-        m[i] = c%19
-        c=c/10
-    sum=0
-    
+def RandomElemInLem(s):
+    L=[]
+    print (" ")
+    for i in range(int(s)): 
+      elem=int(input(("L[i] = ")))
+      L.append(elem)
+    print (" ")
+    print (L)
+    print (" ")
+    random.shuffle (L)
+    print (L)
+    print (" ")
+s=int(input("Размер списка: "))
+print (" ")
+RandomElemInLem(s)
