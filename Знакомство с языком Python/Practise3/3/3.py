bb87cf05ac0f02f8eda4ad2ebe52151a880d637b
and88rus <nest160802@gmail.com>
2022-09-19 02:28:50 +0300

    сделал поправки

--- Знакомство с языком Python/Practise3/3/3.py
+++ Знакомство с языком Python/Practise3/3/3.py
@@ -1,4 +1,26 @@
+from decimal import Decimal
+import math
 def print_hi():
     print()
 if __name__ == '__main__':
     print_hi()
+def CreationOfMassive(s, L):
+   print (" ")
+   for i in range(int(s)):
+     elem=float(input((f'L[{i}] = ')))
+     L.append(elem)
+def OutPutOfMassive(s,L):
+    print (" ")
+    print (str(L).strip('[]'))
+L=[]
+s=int(input("Размер списка: "))
+CreationOfMassive(s,L)
+OutPutOfMassive (s,L)  
+d=2.005
+a=str(d)
+x = Decimal(str(d))
+y = int(float(str(d)))
+z=x-y
+print (x)
+print (y)
+print (z)
