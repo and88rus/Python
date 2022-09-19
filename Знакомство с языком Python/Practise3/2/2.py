b157a3e45a3dd3a7f3f50ee0cdfc529474035369
and88rus <nest160802@gmail.com>
2022-09-19 04:21:21 +0300

    кое что подправил

--- Знакомство с языком Python/Practise3/2/2.py
+++ Знакомство с языком Python/Practise3/2/2.py
@@ -1,26 +1,38 @@
+from decimal import Decimal
+import math
 def print_hi():
     print()
 if __name__ == '__main__':
     print_hi()
-def CreationOfMassive(s, L):
+def CreationOfListL1(s, L1):
    print (" ")
    for i in range(int(s)):
-     elem=int(input(("L[i] = ")))
-     L.append(elem)
-def OutPutOfMassive(s,L):
+     elem=float(input((f'L[{i}] = ')))
+     L1.append(elem)
+def OutPutOfListL1(s,L1):
     print (" ")
-    print (str(L).strip('[]'))
-def SumOfElem(s, L):
-    sum=0
-    for i in range(int(s)): 
-          if (i%2==1):
-            sum+=L[i]
-    print (" ")
-    print ("Сумма элементов на нечетных позициях:")
-    print (" ")
-    print (sum)
-L=[]
+    print (str(L1).strip('[]'))
+def CreationOfListL2(s, L1, L2):
+    for i in range (int(s)):
+        elem=float(Decimal(str(L1[i]))-int(float(str(L1[i]))))
+        L2.append(elem)
+def MinOfL2(s,L2):
+    min=L2[0]
+    for i in range (int(s)):
+     if ((L2[i]<min)&(L2[i]!=0.0)):
+        min=L2[i]
+    return (min)
+def MaxOfL2(s,L2):
+    max=L2[0]
+    for i in range (int(s)):
+     if (L2[i]>max):
+        max=L2[i]
+    return (max)
+L1=[]
+L2=[]
 s=int(input("Размер списка: "))
-CreationOfMassive(s,L)
-OutPutOfMassive (s,L)
-SumOfElem(s, L)
+CreationOfListL1(s,L1)
+OutPutOfListL1 (s,L1) 
+CreationOfListL2(s, L1, L2)
+print (" ")
+print (abs(MaxOfL2(s,L2)-MinOfL2(s,L2)))
--- Знакомство с языком Python/Practise3/3/3.py
+++ Знакомство с языком Python/Practise3/3/3.py
@@ -4,23 +4,35 @@ def print_hi():
     print()
 if __name__ == '__main__':
     print_hi()
-def CreationOfMassive(s, L):
+def CreationOfListL1(s, L1):
    print (" ")
    for i in range(int(s)):
      elem=float(input((f'L[{i}] = ')))
-     L.append(elem)
-def OutPutOfMassive(s,L):
+     L1.append(elem)
+def OutPutOfListL1(s,L1):
     print (" ")
-    print (str(L).strip('[]'))
-L=[]
+    print (str(L1).strip('[]'))
+def CreationOfListL2(s, L1, L2):
+    for i in range (int(s)):
+        elem=float(Decimal(str(L1[i]))-int(float(str(L1[i]))))
+        L2.append(elem)
+def MinOfL2(s,L2):
+    min=L2[0]
+    for i in range (int(s)):
+     if ((L2[i]<min)&(L2[i]!=0.0)):
+        min=L2[i]
+    return (min)
+def MaxOfL2(s,L2):
+    max=L2[0]
+    for i in range (int(s)):
+     if (L2[i]>max):
+        max=L2[i]
+    return (max)
+L1=[]
+L2=[]
 s=int(input("Размер списка: "))
-CreationOfMassive(s,L)
-OutPutOfMassive (s,L)  
-d=2.005
-a=str(d)
-x = Decimal(str(d))
-y = int(float(str(d)))
-z=x-y
-print (x)
-print (y)
-print (z)
+CreationOfListL1(s,L1)
+OutPutOfListL1 (s,L1) 
+CreationOfListL2(s, L1, L2)
+print (" ")
+print (abs(MaxOfL2(s,L2)-MinOfL2(s,L2)))
