d48f11030ee2232a6f2a0dd2599f09de2642936f
and88rus <nest160802@gmail.com>
2022-09-26 19:14:26 +0300

    сделал 6-ое задание

--- Знакомство с языком Python/Practise2/6/6.py
+++ Знакомство с языком Python/Practise2/6/6.py
@@ -2,3 +2,15 @@ def print_hi():
     print()
 if __name__ == '__main__':
     print_hi()
+def In(str,substr):
+    k=0
+    for i in range(len(str)-len(substr)+1):
+        if str[i:i+len(substr)]==substr:
+            k+=1
+    print (f'Количество вхождений "{substr}" в "{str}": ' ,k)
+
+str=input("Основная строка: ")
+print (" ")
+substr=input("Подстрока по отношению к основной: ")
+print (" ")
+In(str,substr)
