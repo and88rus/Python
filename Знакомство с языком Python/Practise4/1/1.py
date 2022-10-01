7326acb8a5887f5dfc998fc966d9a1c0e23e22b1
and88rus <nest160802@gmail.com>
2022-10-01 21:45:54 +0300

    сделал 1-ое задание по 4-ой практической

---
+++ Знакомство с языком Python/Practise4/1/1.py
@@ -0,0 +1,17 @@
+import math
+def print_hi():
+    print()
+if __name__ == '__main__':
+    print_hi()
+    def Pi(d):
+        k=0
+        while ((float.is_integer(d))==False):
+         d*=10
+         k=k+1
+        return(round(math.pi, k))
+    d = float(input("d = "))
+    print (" ")
+    if (0.00000000001<=d<=0.1):
+     print (f'Pi = {Pi(d)} c точностью d = {d}')
+    else: 
+     print ("d<0.00000000001 or d>0.1")
