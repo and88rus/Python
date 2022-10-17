7fc4db010fd8503e450f6c81916fa832e1047041
and88rus <nest160802@gmail.com>
2022-10-17 15:38:23 +0300

    changes at 15:37 17.10.2022

--- AMCS/Practise/2/2.py
+++ AMCS/Practise/2/2.py
@@ -1,4 +1,49 @@
+import random
+
+
+
+
 def print_hi():
     print()
 if __name__ == '__main__':
     print_hi()
+
+
+
+
+def creation_of_L(L,n):
+    for i in range(int(n)):
+        elem=random.random()*random.randint(a,b)
+        L.append(elem)
+
+
+
+
+def output_of_L(L):
+    print (L)
+
+
+
+
+L=[]
+
+
+
+n = int(input("n = "))
+print (" ")
+
+
+a = int(input("a = "))
+print (" ")
+
+
+b = int(input("b = "))
+print (" ")
+
+
+
+creation_of_L(L,n)
+
+
+
+output_of_L (L)
--- AMCS/Practise/6/6.py
+++ AMCS/Practise/6/6.py
@@ -2,3 +2,18 @@ def print_hi():
     print()
 if __name__ == '__main__':
     print_hi()
+
+
+
+
+def cout_of_words():
+    k=0 
+  for i 
+  if ():
+    k=+1
+return k
+
+
+
+
+str = input()
