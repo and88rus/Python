b76139b01e5ba83614cfc72ac7d6fef46d8eb363
and88rus <nest160802@gmail.com>
2022-10-18 22:20:19 +0300

    сделал 6-ое задание

--- AMCS/Practise/6/6.py
+++ AMCS/Practise/6/6.py
@@ -6,14 +6,39 @@ if __name__ == '__main__':
 
 
 
-def cout_of_words():
-    k=0 
-  for i 
-  if ():
-    k=+1
-return k
+def cout_of_words(str):
+  k=0 
+  x=' '
+  string=list(str)
+  for i in range (1,len(string)-1):
+   if (string[len(string)-1]!=x):
+    string.append(' ')
+  for i in range (1,len(string)):
+    if ((string[i]==x) & (string[i-1]!=x)):
+     k=k+1
+  return (k)
+
+
+
+
+print ("Введите строку:")
+
+
+
+
+print (" ")
 
 
 
 
 str = input()
+
+
+
+
+print (" ")
+
+
+
+
+print (f'Кол-во введенных слов - {cout_of_words(str)}.')
