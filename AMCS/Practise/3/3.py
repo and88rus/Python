e742900a62e537547910c99ea70d09a49726be69
and88rus <nest160802@gmail.com>
2022-10-28 10:56:57 +0300

    доделал 4-ое задание

--- AMCS/Practise/3/3.py
+++ AMCS/Practise/3/3.py
@@ -12,14 +12,20 @@ def last_number_of_n(n):
 
 
 
+def last_2_numbers_of_n(n):
+  return n%100
+
+
+
+
 def finish(n):
-  if (last_number_of_n(n)==0):
+  if (last_2_numbers_of_n(n)==11 or last_2_numbers_of_n(n)==12 or last_2_numbers_of_n(n)==13 or last_2_numbers_of_n(n)==14):
     return " "
   elif (last_number_of_n(n)==1):
     return "а"
   elif (last_number_of_n(n)==2 or last_number_of_n(n)==3 or last_number_of_n(n)==4):
     return "ы"
-  elif (last_number_of_n(n)==5 or last_number_of_n(n)==6 or last_number_of_n(n)==7 or last_number_of_n(n)==8 or last_number_of_n(n)==9): 
+  elif (last_number_of_n(n)==0 or last_number_of_n(n)==5 or last_number_of_n(n)==6 or last_number_of_n(n)==7 or last_number_of_n(n)==8 or last_number_of_n(n)==9): 
     return " "
 
 
