965a40295fc79f092c67c4a4a39f94a71cd3821c
and88rus <nest160802@gmail.com>
2022-11-02 02:05:15 +0300

    почти сделал 4-ое задание

--- Знакомство с языком Python GB/Practise4/4/4,0.py
+++ Знакомство с языком Python GB/Practise4/4/4,0.py
@@ -1,3 +1,8 @@
+import random
+
+
+
+
 def print_hi():
     print()
 if __name__ == '__main__':
@@ -6,11 +11,18 @@ if __name__ == '__main__':
 
 
 
-def List(L): 
+def List(L,k,i): 
+   a=0
+   b=0
    unL=[]
-   for i in range (0, 2):
-    a = int(input("a = "))
-    unL.append (a)
+   if (i==0):
+    b = k
+    a = random.randint(0,100)
+   else:
+    a = random.randint(0,100)
+    b = random.randint(0,k)
+   unL.append (a)
+   unL.append (b)
    print (unL)
    L.append(unL)
 
@@ -45,11 +57,118 @@ def Itog(L3, a, b,L4):
 
 
 
-def Symb(L4):
-    if ((L4[i][0]>0) and (i!=0)):
-        return "+"
-    elif (i==0):
-        return ""
+
+def voz(L4):
+  if ((L4[i][0]==0) and (L4[i][1])==0):
+     return ""
+  elif ((L4[i][0]==0) and (L4[i][1])==1):
+     return ""
+  elif ((L4[i][0]==1) and (L4[i][1])==0):
+     return ""
+  elif ((L4[i][0]==1) and (L4[i][1])==1):
+     return ""
+  elif ((L4[i][0]>1) and (L4[i][1])==0):
+     return ""
+  elif ((L4[i][0]>1) and (L4[i][1])==1):
+     return ""
+  elif ((L4[i][0]==0) and (L4[i][1])>1):
+     return ""
+  elif ((L4[i][0]==1) and (L4[i][1])>1):
+     return "^"
+  elif ((L4[i][0]>1) and (L4[i][1])>1):
+     return "^"
+  
+  
+ 
+
+  
+
+def deg(L4):
+  if ((L4[i][0]==0) and (L4[i][1])==0):
+     return ""
+  elif ((L4[i][0]==0)and (L4[i][1])==1):
+     return ""
+  elif ((L4[i][0]==1)and (L4[i][1])==0):
+     return ""
+  elif ((L4[i][0]==1) and (L4[i][1])==1):
+     return ""
+  elif ((L4[i][0]>1) and (L4[i][1])==0):
+     return ""
+  elif ((L4[i][0]>1)and (L4[i][1])==1):
+     return ""
+  elif ((L4[i][0]==0) and (L4[i][1])>1):
+     return ""
+  elif ((L4[i][0]==1) and (L4[i][1])>1):
+     return str(L4[i][1])
+  elif ((L4[i][0]>1) and (L4[i][1])>1):
+     return str(L4[i][1])
+ 
+  
+
+  
+def x(L4):
+  if ((L4[i][0]==0) and (L4[i][1])==0):
+     return ""
+  elif ((L4[i][0]==0) and (L4[i][1])==1):
+     return ""
+  elif ((L4[i][0]==1) and (L4[i][1])==0):
+     return ""
+  elif ((L4[i][0]==1) and (L4[i][1])==1):
+     return "x"
+  elif ((L4[i][0]>1) and (L4[i][1])==0):
+     return ""
+  elif ((L4[i][0]>1)and (L4[i][1])==1):
+     return "x"
+  elif ((L4[i][0]==0) and (L4[i][1])>1):
+     return ""
+  elif ((L4[i][0]==1) and (L4[i][1])>1):
+     return "x"
+  elif ((L4[i][0]>1) and (L4[i][1])>1):
+     return "x"
+ 
+
+
+ 
+def c(L4):
+  if ((L4[i][0]==0) and (L4[i][1])==0):
+     return ""
+  elif ((L4[i][0]==0) and (L4[i][1])==1):
+     return ""
+  elif ((L4[i][0]==1) and (L4[i][1])==0):
+     return ""
+  elif ((L4[i][0]==1) and (L4[i][1])==1):
+     return ""
+  elif ((L4[i][0]>1) and (L4[i][1])==0):
+     return str(L4[i][0])
+  elif ((L4[i][0]>1)and (L4[i][1])==1):
+     return str(L4[i][0])
+  elif ((L4[i][0]==0) and (L4[i][1])>1):
+     return ""
+  elif ((L4[i][0]==1) and (L4[i][1])>1):
+     return ""
+  elif ((L4[i][0]>1) and (L4[i][1])>1):
+     return str(L4[i][0])
+
+
+
+def sort_of_L4_up(L4): 
+    t=0
+    for j in range(0,len(L4)-1):
+        for i in range(0,len(L4)-j-1):
+            if (L4[i][1] < L4[i+1][1]):
+                t=L4[i]
+                L4[i]=L4[i+1]
+                L4[i+1]=t
+
+
+
+
+def plus(i,L4):
+    if (i!=len(L4)-1):
+        return (" + ")
+    else: 
+        return ('')
+
 
 
 
@@ -82,16 +201,13 @@ k = int(input("k = "))
 
 
 
-for i in range (0, int(k)):
- List(L)
+for i in range (0, int(k)+1):
+ List(L,k,i)
 
 
 
 print ("")
 
-
-
-List(L)
 
 print (L)
 
@@ -120,9 +236,40 @@ print ("")
 for i in range (0, len(L2)):
     Itog(L3,L1[i],L2[i],L4)
 
+
+sort_of_L4_up(L4)
+
+for i in range (0, len(L4)):
+    L4[i][0]=random.randint(0,100)
+
 print (L4)
 
 print ("")
 
+
 for i in range (0,len(L4)):
-    print (f'{Symb(L4)}{L4[i][0]}')
+    print (f'{c(L4)}{x(L4)}{voz(L4)}{deg(L4)}{plus(i,L4)}',end = " ")
+    if (i==len(L4)-1):
+         print ("= 0")
+print()
+
+
+
+
+data = open('for 4 of 4.txt','w')
+
+for i in range (0,len(L4)):
+  data.write(c(L4))
+  data.write(x(L4))
+  data.write(voz(L4))
+  data.write(deg(L4))
+  data.write(plus(i,L4))
+  if (i==len(L4)-1):
+         data.write (" = 0")
+
+data.close()
+
+exit()
+
+
+
